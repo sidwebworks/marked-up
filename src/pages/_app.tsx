5b3a0843f64cec7d11ee4f9c5ee40cba4586a34d
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-06-11 15:43:16 +0530

    chore: update title and add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { Provider as JotaiProvider } from "jotai";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 import { Toaster } from "react-hot-toast";
 import { EditorProvider } from "src/hooks/use-editor";
 import "windi.css";
@@ -7,12 +9,40 @@ import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <JotaiProvider>
-      <EditorProvider>
-        <Component {...pageProps} />
-      </EditorProvider>
-      <Toaster />
-    </JotaiProvider>
+    <Fragment>
+      <Head>
+        <title>Marked UP</title>
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="black"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="author" content="Sidharth Rathi" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="notranslate" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="otherbot" content="index, follow" />
+        <meta name="google" content="nopagereadaloud" />
+        <meta name="copyright" content="Sidharth Rathi" />
+        <meta name="Classification" content="Web application" />
+        <meta name="designer" content="Sidharth Rathi" />
+        <meta name="owner" content="Sidharth Rathi" />
+        <meta
+          name="description"
+          content="A better, feature rich README editor and generator"
+        />
+      </Head>
+      <JotaiProvider>
+        <EditorProvider>
+          <Component {...pageProps} />
+        </EditorProvider>
+        <Toaster />
+      </JotaiProvider>
+    </Fragment>
   );
 }
 
